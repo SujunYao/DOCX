1691b3862b092683b4401ac2d4bcaf3bc46664f1
Adam Yao <Adamyao@yeah.net>
2020-03-20 11:27:41 +0800

    [DEV]

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,6 @@
 
 import { Express, Request, Response } from "express";
 import path from 'path';
-import express from 'express';
 
 export class Server {
 
@@ -9,14 +8,15 @@ export class Server {
 
     constructor(app: Express) {
         this.app = app;
-        this.app.use(express.static(path.resolve("./") + "/build/frontend"));
-
+        app.all('*', function (req, res, next) {
+            // res.header("Access-Control-Allow-Origin", "http://192.168.0.168:4500");
+            res.header("Access-Control-Allow-Origin", "http://10.193.17.26:3000");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Methods", "PUT,POST,GET");
+            next();
+        });
         this.app.get("/api", (req: Request, res: Response): void => {
             res.send("You have reached the API!");
-        });
-    
-        this.app.get("*", (req: Request, res: Response): void => {
-            res.sendFile(path.resolve("./") + "/build/frontend/index.html");
         });
     }
 
