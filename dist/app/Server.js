3e44f6e34b582391a262275271e6038b9eca6499
Adam Yao <Adamyao@yeah.net>
2020-03-20 10:39:51 +0800

    [DEV]

---
+++ dist/app/Server.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Server {
+    constructor(app) {
+        this.app = app;
+        this.app.get("/api", (req, res) => {
+            res.send("You have reached the API!");
+        });
+    }
+    start(port) {
+        this.app.listen(port, () => console.log(`Server listening on port ${port}!`));
+    }
+}
+exports.Server = Server;
